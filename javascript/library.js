305bd942cba96ecc792f437364a0c2303dc7e283
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-06-05 21:41:04 -0400

    Set up basic opening and closing of the form, and started setting up js for form

--- javascript/library.js
+++ javascript/library.js
@@ -1,9 +1,29 @@
-const bookBtn = document.getElementById("bookBtn");
+const bookBtn = document.getElementById("book-btn");
+const form = document.getElementById("form");
+const libraryMain = document.querySelector(".library-main");
+const formCancelBtn = document.getElementById("cancel-btn");
+const formAddBtn = document.getElementById("add-btn");
+
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("read");
+const bookNotRead = document.getElementById("not-read");
 
 const myLibrary = [];
 
 bookBtn.addEventListener("click", function () {
-  console.log("clicked");
+  form.style.visibility = "visible";
+  libraryMain.style.visibility = "hidden";
+});
+
+formCancelBtn.addEventListener("click", function () {
+  form.style.visibility = "hidden";
+  libraryMain.style.visibility = "visible";
+});
+
+formAddBtn.addEventListener("click", function () {
+  console.log("Book title: " + bookTitle.value);
 });
 
 function Book() {
